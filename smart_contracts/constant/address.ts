1ed8e8364f3b9f2f5850db83d6ca1de451b1dc1f
khmalz <akmalrma1710@gmail.com>
2025-02-04 09:14:57 +0700

    add env to save contract address

--- smart_contracts/constant/address.ts
+++ smart_contracts/constant/address.ts
@@ -1,1 +1,5 @@
-export const contractAddress: string = "0xAfe1b5bdEbD4ae65AF2024738bf0735fbb65d44b";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+export const contractAddress: string = process.env.CONTRACT_ADDRESS || "";
